5b20ace14cede661d30f6358715d03c2fdc10826
wolerdev <gabrielgalati.js@gmail.com>
2022-10-08 16:09:55 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+export class tinderPagination {
+    personas = [
+        {
+            id: 1,
+            name: 'John',
+            age: 25,
+        },
+        {
+            id: 2,
+            name: 'Jane',
+            age: 26,
+
+        },
+        {
+            id: 3,
+            name: 'Jack1',
+        },
+
+        //method
+   
+    ];
+    actualPage = 1;
+    IsbackPossible = false;
+    next(){
+        this.actualPage++;
+        this.IsbackPossible = true;
+    }
+    previous(){
+        
+        if(this.actualPage > 1){
+            if( this.IsbackPossible){
+                this.actualPage--;
+            }
+        }
+        this.IsbackPossible = false;
+    }
+    // newsPerson(personas){
+    //     const lastPerson = this.personas[this.personas.length - 1];
+    //     const newPersons = personas.unshift(lastPerson);
+    //     return this.personas = personas
+    // }
+    newsPerson(personas){
+        const lastPerson = this.personas[this.personas.length - 1];
+         personas.unshift(lastPerson);
+        return this.personas = personas
+    }
+    getPersonas(){
+        console.log(this.personas)
+        return this.personas;
+    }
+}
+
+
+const tinderP = new tinderPagination();
+tinderP.previous();
+tinderP.next();
+tinderP.next();
+tinderP.next();
+tinderP.previous();
+
+tinderP.previous();
+tinderP.previous();
+tinderP.previous();
+tinderP.previous();
+tinderP.newsPerson([
+    {
+        id: 4,
+        name: 'Jhon',
+    },
+    {
+        id: 5,
+        name: 'Jhon',
+    }
+]);
+tinderP.getPersonas();
+
+
+
---
+++ test/index.test.js
@@ -0,0 +1,61 @@
+import {tinderPagination} from '../index.js';
+
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+
+test('tinderPagination next page', () => {
+    const tinderP = new tinderPagination();
+    tinderP.next();
+    assert.is(tinderP.actualPage, 2);
+});
+
+test('tinderPagination previous page', () => {
+    const tinderP = new tinderPagination();
+    tinderP.next();
+    tinderP.previous();
+    assert.is(tinderP.actualPage, 1);
+    tinderP.next();
+    tinderP.next();
+    tinderP.next();
+    tinderP.next();
+    tinderP.previous();
+    tinderP.previous();
+    tinderP.previous();
+    tinderP.previous();
+    tinderP.previous();
+    tinderP.previous();
+    tinderP.previous();
+    tinderP.previous();
+    assert.is(tinderP.actualPage, 4);
+});
+
+test('tinderPagination newsPerson', () => {
+    const tinderP = new tinderPagination();
+    tinderP.newsPerson([
+        {
+            id: 4,
+            name: 'Jack1',
+
+        },
+    ]);
+    let personas = tinderP.getPersonas();
+    assert.is(JSON.stringify(personas), 
+    
+    JSON.stringify(
+        [
+            {
+                        id: 3,
+                        name: 'Jack1',
+                    },
+                    {
+                        id: 4,
+                        name: 'Jack1',
+                    }
+        ]));
+
+
+
+
+});
+
+test.run();
